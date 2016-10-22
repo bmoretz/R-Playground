67844dbfb1a8bafe6980cbcb281f719c665b4d9c
Brandon Moretz <bmoretz@ionicsolutions.net>
2016-10-22 15:53:48 -0400

    Updated project layout.

--- R Playground/IntoToStats/ch2/ch2.2.4.R
+++ R Playground/IntoToStats/ch2/ch2.2.4.R
@@ -1,4 +1,11 @@
 x <- rnorm(100)
 
+# inaccurate combined render
 hist(x, freq = F)
+curve(dnorm(x), add = T)
+
+# accurate combined render
+h <- hist(x, plot = F)
+ylimit <- range(0, h$density, dnorm(0))
+hist(x, freq = F, ylim = ylimit)
 curve(dnorm(x), add = T)
