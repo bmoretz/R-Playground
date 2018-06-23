591b6d675b6ec6768aee0e8368299a4f3241a073
Brandon Moretz <bmoretz@ionicsolutions.net>
2018-06-23 18:03:59 -0400

    R in Action, ch2
    R in Action, ch2

---
+++ BookWork/RInAction/ch_02/2.3.R
@@ -0,0 +1,13 @@
+# Keyboard Entry
+
+mydata <- data.frame(age = numeric(0), gender = character(0), weight = numeric(0))
+mydata <- edit(mydata)
+
+# Inline
+
+mydatatxt <- "
+age gender weight
+25 m 166
+60 f 115
+18 f 120"
+
---
+++ Scripts/RSQL/funda_example.sql
@@ -0,0 +1,4 @@
+﻿SELECT TOP 10 
+	[fyear]
+FROM
+	dbo.[funda]
