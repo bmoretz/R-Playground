8200fcd90f81648396ad3b85d7b4911e119be761
Brandon Moretz <bmoretz@ionicsolutions.net>
2018-07-01 17:59:44 -0400

    MSDS401, Optional assignment #2.
    MSDS401, Optional assignment #2.

--- Classes/MSDS401/Exercises/01_into/ex_01.R
+++ Classes/MSDS401/Exercises/01_into/ex_01.R
@@ -10,7 +10,7 @@ data_path <- "data/"
 data.raw <- as.data.table(fread(paste0(data_path, "home_prices.csv"),
   header = TRUE, stringsAsFactors = FALSE, na.strings = c("NA", "")))
 
-home.sales <- setNames(data.raw, c("Price", "Sqft", "Year", "Baths", "Feats", "NBR", "Corner", "Tax"))
+home.sales <- setNames(data.raw, tools::toTitleCase(tolower(names(data.raw))))
 
 str(home.sales)
 
--- Classes/MSDS401/Exercises/02_charts/ex_02.R
+++ Classes/MSDS401/Exercises/02_charts/ex_02.R
@@ -3,12 +3,21 @@ library(ggplot2)
 library(ggthemes)
 library(gridExtra)
 
-theme_set(theme_economist())
+theme_set(theme_fivethirtyeight())
 
-data_path <- "/data/"
+data.path <- "C:/Projects/R/Playground/Classes/MSDS401/Exercises/02_charts/data/"
 
-data.raw <- as.data.table(fread(paste0(data_path, "home_prices.csv"),
+data.raw <- as.data.table(fread(paste0(data.path, "home_prices.csv"),
   header = TRUE, stringsAsFactors = FALSE, na.strings = c("NA", "")))
 
 home.sales <- setNames(data.raw, tools::toTitleCase(tolower(names(data.raw))))
 
+home.sales$Year <- as.Date(paste(home.sales$Year, 12, 31, sep = "-"))
+home.sales$Baths <- factor(home.sales$Baths, ordered = TRUE)
+home.sales$Feats <- factor(home.sales$Feats, ordered = TRUE)
+home.sales$Nbr <- home.sales$Nbr == "YES"
+home.sales$Corner <- home.sales$Corner == "YES"
+
+glimpse(home.sales)
+
+?
