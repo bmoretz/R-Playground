62f658a9b4462788d575e47816fa7eb6c89a5c09
Brandon Moretz <bmoretz@ionicsolutions.net>
2018-07-05 16:55:35 -0400

    401 Study Work
    401 Study Work

--- Classes/MSDS401/Exercises/03_descriptive_stats/_scratch.R
+++ Classes/MSDS401/Exercises/03_descriptive_stats/_scratch.R
@@ -1,45 +1,7 @@
+theme_set(theme_fivethirtyeight())
 
-milage.raw <- as.data.table(fread(paste0(data.path, "mileage.csv"),
-  header = TRUE, stringsAsFactors = FALSE, na.strings = c("NA", "")))
-
-milage <- setNames(milage.raw, tools::toTitleCase(tolower(names(milage.raw))))
-
-milage$Class <- factor(milage$Class, ordered = TRUE)
-
-
-loadDataFile <- function(file_name) {
-	data.raw <- as.data.table(fread(paste0(data.path, file_name),
-		header = TRUE, stringsAsFactors = FALSE, na.strings = c("NA", "")))
-
-	data <- setNames(data.raw, tools::toTitleCase(tolower(names(data.raw))))
-
-	return(data)
-}
-
-getMode <- function(values) {
-	tbl.vals <- data.frame(table(values))
-	tbl.vals$values <- as.numeric(levels(tbl.vals$values))[tbl.vals$values]
-	mode <- tbl.vals[which.max(tbl.vals$Freq),]
-
-	names(mode)[1] <- "Value"
-
-	mode
-}
-
-getSummary <- function(values) {
-	mode <- getMode(values)
-
-	data.table(
-		Mean = mean(values),
-		Median = median(values),
-		Min = min(values),
-		Max = max(values),
-		Range = max(values) - min(values),
-		Sum = sum(values),
-		Mode = mode[1,],
-		Count = length(values),
-		StdDev = sd(values),
-		Variance = var(values),
-	)
-}
-
+# Theme Overrides
+theme_update(plot.title = element_text(hjust = 0.5),
+             axis.text.x = element_text(face = "bold", size = 12),
+             axis.text.y = element_text(face = "bold", size = 12),
+             legend.position = "top", legend.title = element_blank())
