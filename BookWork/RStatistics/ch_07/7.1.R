8012992e1e3993bef905d1fe2488f7dd14902811
Brandon Moretz <bmoretz@ionicsolutions.net>
2018-07-15 16:00:04 -0400

    401 Optional R Lesson #5
    401 Optional R Lesson #5

--- BookWork/RStatistics/ch_07/7.1.R
+++
@@ -1,5 +0,0 @@
-
-# Suppose that a fair die is rolled 10 times. What is the probability of thkrowing exactly two sixes?
-
-dbinom(2, size = 10, 1 / 6)
-
--- Classes/MSDS401/Practice/module_03/ch4.R
+++ Classes/MSDS401/Practice/module_03/ch4.R
@@ -1,4 +1,3 @@
-x <- c(0, 1, 2, 3, 4, 5, 6)
 
 binomial <- dbinom(x, 100, 0.01, log = FALSE)
 
@@ -6,3 +5,11 @@ hist(binomial, breaks = pretty(binomial))
 
 dpois(x, 1.0, log = FALSE)
 dhyper(x, 20, 2000, 100, log = FALSE)
+
+pbinom(10, size = 23, prob = .64)
+dbinom(10, size = 23, prob = .64)
+
+n <- 20
+p <- .86
+q <- 1 - p
+sqrt(n * q * p)
