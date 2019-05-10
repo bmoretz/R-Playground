16a6decf18654cf572c108d9773e70f8b4c542e3
Brandon Moretz <bmoretz@ionicsolutions.net>
2019-05-10 14:51:48 -0400

    MLwR, ch3
    MLwR, ch3

--- BookWork/MachineLearningWithR/Chapter02/ch_02_work.R
+++ BookWork/MachineLearningWithR/Chapter02/ch_02_work.R
@@ -14,3 +14,42 @@ quantile(usedcars$price, probs = c(0.01, .99))
 
 quantile(usedcars$price, seq(from = 0, to = 1, by = .2))
 
+qqnorm(usedcars$price)
+
+boxplot(usedcars$price, main = "Boxplot of Used Car Prices",
+        ylab = "Price ($)")
+
+boxplot(usedcars$mileage, main = "Boxplot of Used Car Milage",
+        ylab = "Odometer (mi.)")
+
+hist(usedcars$price, main = "Histogram of Used Car Prices",
+     xlab = "Price ($)")
+
+hist(usedcars$mileage, main = "Boxplot of Used Car Milage",
+        xlab = "Odometer (mi.)")
+
+var(usedcars$price)
+sd(usedcars$price)
+
+var(usedcars$mileage)
+sd(usedcars$mileage)
+
+table(usedcars$year)
+
+table(usedcars$model)
+
+table(usedcars$color)
+
+model_table <- table(usedcars$model)
+prop.table(model_table)
+
+color_table <- table(usedcars$color)
+color_pct <- prop.table(color_table) * 100
+round(color_pct, digits = 1)
+
+plot(x = usedcars$mileage, y = usedcars$price,
+     main = "Scatterplot of Price vs. Milage",
+     xlab = "Used Car Odometer (mi.)",
+     ylab = "Used Car Price ($)") +
+     abline(lm(price ~ mileage, usedcars))
+
